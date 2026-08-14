338242ace85f1cb9ae64d886b8ec4a4d6007ff8e
Stephen Piccolo <stephen_piccolo@byu.edu>
2026-08-14 16:33:04 -0600

    Debugging BrainArray download

--- Affymetrix_RNA_Microarray/install_BrainArray_packages.R
+++ Affymetrix_RNA_Microarray/install_BrainArray_packages.R
@@ -30,8 +30,9 @@ download_brainarray_file = function(url, destfile) {
       "--max-time", "1800",
       "--speed-time", "30",
       "--speed-limit", "1000",
-      "-H", "Connection: close",
-      "-C", "-",
+      "--http1.0",
+      "--header=Connection:close",
+      "-C-",
       "-o", destfile,
       url
     )
