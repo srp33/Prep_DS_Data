12b321867ddda4b3aa017f9b032bad269f9dc335
Stephen Piccolo <stephen_piccolo@byu.edu>
2026-08-14 16:15:16 -0600

    Debugging BrainArray download

--- Affymetrix_RNA_Microarray/install_BrainArray_packages.R
+++ Affymetrix_RNA_Microarray/install_BrainArray_packages.R
@@ -4,21 +4,19 @@
 library(tidyverse)
 library(readxl)
 
-annotationSource = "entrezg"
+#annotationSource = "entrezg"
+annotationSource = "ensg"
 version = "25.0.0"
 
 # Split cells that list multiple BrainArray platforms (semicolon-separated),
 # then collect the unique platform values.
 brainarrayPlatforms = read_xlsx("/project/datasets.xlsx") %>%
   filter(!is.na(brainarray), brainarray != "") %>%
-  # filter(brainarray != "hta20hs") %>% # This is a temporary exclusion until we can download this package from BrainArray.
   separate_rows(brainarray, sep = ";") %>%
   mutate(brainarray = str_trim(brainarray)) %>%
   filter(brainarray != "") %>%
   distinct(brainarray) %>%
   pull(brainarray)
-
-brainarrayPlatforms = c("hta20hs")
 
 for (platform in brainarrayPlatforms) {
   packageUrl = paste0(
@@ -30,7 +28,6 @@ for (platform in brainarrayPlatforms) {
   tempPackageFileName = basename(packageUrl)
   tempPackageFilePath = paste0(tempdir(), "/", tempPackageFileName)
 
-print(packageUrl)
   download.file(packageUrl, tempPackageFilePath)
 
   install.packages(tempPackageFilePath, repos = NULL, type = "source")
